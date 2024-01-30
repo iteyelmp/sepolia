2628f81724ca81d69e214c2cb49efb00f105f4ce
iteyelmp@gmail.com <lmp19890127>
2024-01-30 10:54:09 +0800

    test

--- geth/geth.sh
+++ geth/geth.sh
@@ -10,17 +10,13 @@
 #  --networkid "$network_id" \
 #  --syncmode=full \
 #  --verbosity ${EL_LOG_LEVL:-3} \
-#  --authrpc.jwtsecret /config/jwtsecret \
-#  --http \
-#  --http.addr=0.0.0.0 \
-#  --http.vhosts=* \
-#  --authrpc.vhosts="*" \
-#  --authrpc.addr=0.0.0.0 \
-#  --port=${EL_P2P_PORT:-30303} \
 #  --bootnodes "$bootnodes"
 
 exec geth \
   --sepolia \
+  --datadir /db \
+  --syncmode=full \
+  --verbosity ${EL_LOG_LEVL:-3} \
   --authrpc.jwtsecret /config/jwtsecret \
   --http \
   --http.addr=0.0.0.0 \
