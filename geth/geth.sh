ef737aeefbdea144325f567299103e30780033ec
iteyelmp@gmail.com <lmp19890127>
2024-01-30 10:40:43 +0800

    test

--- geth/geth.sh
+++ geth/geth.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-geth init  --datadir /db /config/genesis.json
-
-bootnodes=$(cat /config/enodes.list)
-network_id=$(jq -r '.config.chainId' /config/genesis.json)
-
+#geth init  --datadir /db /config/genesis.json
+#
+#bootnodes=$(cat /config/enodes.list)
+#network_id=$(jq -r '.config.chainId' /config/genesis.json)
+#
 #exec geth \
 #  --datadir /db \
 #  --networkid "$network_id" \
@@ -18,4 +18,16 @@ network_id=$(jq -r '.config.chainId' /config/genesis.json)
 #  --authrpc.addr=0.0.0.0 \
 #  --port=${EL_P2P_PORT:-30303} \
 #  --bootnodes "$bootnodes"
-exec geth --sepolia console
+
+exec geth \
+  --datadir /db \
+  --sepolia \
+  --syncmode=full \
+  --verbosity ${EL_LOG_LEVL:-3} \
+  --authrpc.jwtsecret /config/jwtsecret \
+  --http \
+  --http.addr=0.0.0.0 \
+  --http.vhosts=* \
+  --authrpc.vhosts="*" \
+  --authrpc.addr=0.0.0.0 \
+  --port=${EL_P2P_PORT:-30303}
