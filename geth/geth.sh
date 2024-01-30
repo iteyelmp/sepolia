97d0dd395b69b91eba5ce5b28087e5a9509c43c3
iteyelmp@gmail.com <lmp19890127>
2024-01-30 10:32:41 +0800

    test

--- geth/geth.sh
+++ geth/geth.sh
@@ -5,16 +5,17 @@ geth init  --datadir /db /config/genesis.json
 bootnodes=$(cat /config/enodes.list)
 network_id=$(jq -r '.config.chainId' /config/genesis.json)
 
-exec geth \
-  --datadir /db \
-  --networkid "$network_id" \
-  --syncmode=full \
-  --verbosity ${EL_LOG_LEVL:-3} \
-  --authrpc.jwtsecret /config/jwtsecret \
-  --http \
-  --http.addr=0.0.0.0 \
-  --http.vhosts=* \
-  --authrpc.vhosts="*" \
-  --authrpc.addr=0.0.0.0 \
-  --port=${EL_P2P_PORT:-30303} \
-  --bootnodes "$bootnodes"
+#exec geth \
+#  --datadir /db \
+#  --networkid "$network_id" \
+#  --syncmode=full \
+#  --verbosity ${EL_LOG_LEVL:-3} \
+#  --authrpc.jwtsecret /config/jwtsecret \
+#  --http \
+#  --http.addr=0.0.0.0 \
+#  --http.vhosts=* \
+#  --authrpc.vhosts="*" \
+#  --authrpc.addr=0.0.0.0 \
+#  --port=${EL_P2P_PORT:-30303} \
+#  --bootnodes "$bootnodes"
+exec geth --sepolia console
